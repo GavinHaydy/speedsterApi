4c50352b9c8a981b0cae62e57136b7b07a988c0f
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-05-09 22:49:10 +0800

    feat: is_sys_user

--- common/init/init.sql
+++ common/init/init.sql
@@ -1,38 +1,40 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE
+EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE sys_user (
-                          id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                          username        VARCHAR(50) UNIQUE NOT NULL,
-                          password        VARCHAR(255) NOT NULL,
-                          nickname        VARCHAR(50),
-                          avatar          TEXT,
-                          email           VARCHAR(100),
-                          phone           VARCHAR(20),
+CREATE TABLE sys_user
+(
+    id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username      VARCHAR(50) UNIQUE NOT NULL,
+    password      VARCHAR(255)       NOT NULL,
+    nickname      VARCHAR(50),
+    avatar        TEXT,
+    email         VARCHAR(100),
+    phone         VARCHAR(20),
 
-                          status          SMALLINT DEFAULT 1,
-                          is_super        BOOLEAN DEFAULT FALSE,
+    status        SMALLINT         DEFAULT 1,
+    is_super      BOOLEAN          DEFAULT FALSE,
+    is_sys_user   BOOLEAN          DEFAULT FALSE,
 
-                          last_login_at   TIMESTAMP,
-                          last_login_ip   VARCHAR(45),
+    last_login_at TIMESTAMP,
+    last_login_ip VARCHAR(45),
 
-                          created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          deleted_at      TIMESTAMP
+    created_at    TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
+    deleted_at    TIMESTAMP
 );
 
-INSERT INTO sys_user (
-    id,
-    username,
-    password,
-    nickname,
-    is_super
-) VALUES (
-             uuid_generate_v4(),
-             'admin',
-             '3BMHz_rEwHpi1xrAG8K9PQ', -- bcrypt加密
-             '超级管理员',
-             TRUE
-         );
+INSERT INTO sys_user (id,
+                      username,
+                      password,
+                      nickname,
+                      is_super,
+                      is_sys_user)
+VALUES (uuid_generate_v4(),
+        'admin',
+        '3BMHz_rEwHpi1xrAG8K9PQ', -- bcrypt加密
+        '超级管理员',
+        TRUE,
+        TRUE);
 
 
 CREATE TABLE role
