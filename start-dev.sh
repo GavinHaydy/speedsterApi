182a95b1522db72277faee4d08b0bc277dfa292d
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-06-10 09:23:09 +0800

    refactor: dev start script && api docs

--- start-dev.sh
+++ start-dev.sh
@@ -2,21 +2,21 @@
 
 case "$1" in
   start)
-    tmux new-session -d -s speedster
+    tmux new-session -d -s speedster -n gateway
 
-    tmux send-keys -t speedster:0 \
+    tmux send-keys -t speedster:gateway \
       'cd app/gateway && air' C-m
 
-    tmux new-window -t speedster:1
-    tmux send-keys -t speedster:1 \
+    tmux new-window -t speedster -n user-api
+    tmux send-keys -t speedster:user-api \
       'cd app/user/api && air' C-m
 
-    tmux new-window -t speedster:2
-    tmux send-keys -t speedster:2 \
-    'cd app/user/user && air' C-m
+    tmux new-window -t speedster -n user-rpc
+    tmux send-keys -t speedster:user-rpc \
+      'cd app/user/user && air' C-m
 
-    tmux new-window -t speedster:3
-    tmux send-keys -t speedster:3 \
+    tmux new-window -t speedster -n role-api
+    tmux send-keys -t speedster:role-api \
       'cd app/role/api && air' C-m
 
 
