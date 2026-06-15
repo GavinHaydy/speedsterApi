1926b6666f30105bdad5f60fc1abb691a2ab9273
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-06-15 23:49:42 +0800

    feat: permission list

---
+++ app/permission/api/internal/handler/docs_route.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"embed"
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest"
+)
+
+//go:embed docs/*.json
+var docFS embed.FS
+
+func RegisterDocRoute(server *rest.Server) {
+	server.AddRoutes([]rest.Route{
+		{
+			Method: http.MethodGet,
+			Path:   "/docs/permission.json",
+			Handler: func(w http.ResponseWriter, r *http.Request) {
+				data, _ := docFS.ReadFile("docs/permission.json")
+				w.Header().Set("Content-Type", "application/json")
+				if _, err := w.Write(data); err != nil {
+					return
+				}
+			},
+		},
+	})
+}
