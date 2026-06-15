e3ec01d02747ddd7e63f58865880b214b8c8adab
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-06-15 22:30:38 +0800

    feat: permission list

---
+++ app/permission/model/syspermissionmodel.go
@@ -0,0 +1,69 @@
+package model
+
+import (
+	"context"
+	"speedsterApi/app/permission/rpc/pb"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ SysPermissionModel = (*customSysPermissionModel)(nil)
+
+type (
+	// SysPermissionModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customSysPermissionModel.
+	SysPermissionModel interface {
+		sysPermissionModel
+		withSession(session sqlx.Session) SysPermissionModel
+		SelectTree(ctx context.Context, req *pb.PermissionTreeReq) ([]*SysPermission, error)
+	}
+
+	customSysPermissionModel struct {
+		*defaultSysPermissionModel
+	}
+)
+
+// NewSysPermissionModel returns a model for the database table.
+func NewSysPermissionModel(conn sqlx.SqlConn) SysPermissionModel {
+	return &customSysPermissionModel{
+		defaultSysPermissionModel: newSysPermissionModel(conn),
+	}
+}
+
+func (m *customSysPermissionModel) withSession(session sqlx.Session) SysPermissionModel {
+	return NewSysPermissionModel(sqlx.NewSqlConnFromSession(session))
+}
+
+func (m *customSysPermissionModel) SelectTree(ctx context.Context, req *pb.PermissionTreeReq) ([]*SysPermission, error) {
+
+	builder := squirrel.
+		Select("*").
+		From(m.table).
+		OrderBy("sort asc", "id asc").
+		PlaceholderFormat(squirrel.Dollar)
+
+	if req.Name != "" {
+		builder = builder.Where(
+			squirrel.Like{
+				"name": "%" + req.Name + "%",
+			},
+		)
+	}
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var list []*SysPermission
+
+	err = m.conn.QueryRowsCtx(
+		ctx,
+		&list,
+		query,
+		args...,
+	)
+
+	return list, err
+}
