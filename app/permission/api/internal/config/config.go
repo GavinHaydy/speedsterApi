7de7f458affa99bd7df19193393fe8458dac2034
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-06-18 15:44:04 +0800

    fix: middleware path err

--- app/permission/api/internal/config/config.go
+++ app/permission/api/internal/config/config.go
@@ -4,6 +4,7 @@
 package config
 
 import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/rest"
 	"github.com/zeromicro/go-zero/zrpc"
 )
@@ -18,4 +19,5 @@ type Config struct {
 	DB            struct {
 		DSN string
 	}
+	Redis redis.RedisConf
 }
